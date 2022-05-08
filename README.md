9ed5bc58b0196c72f6e3d11205e1fe381741f506
OiSiS <oisis@o2.pl>
2022-05-08 16:44:29 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+https://docs.ansible.com/ansible/latest/collections/kubernetes/core/k8s_module.html
+
+ansible-galaxy collection install kubernetes.core
---
+++ run.sh
@@ -0,0 +1,16 @@
+# Check connection for all tests:
+#ansible -i ./hosts all -m ping
+
+# Limit connection test to one host:
+#ansible -i ./hosts all --limit prod-docker-registry -m ping
+
+# Run Ansible on remote host group:
+#ansible-playbook -i ./hosts --limit docker-registry playbook.yaml
+
+# Get all facts from remote machine:
+#ansible -i ./hosts all --limit prod-docker-registry -m ping
+
+
+
+# Ansible dry run:
+ansible-playbook playbook.yaml --connection=local
